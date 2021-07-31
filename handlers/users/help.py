0f3e2bdb3dcd41b8ff0bc059a6e28ca56c6b50a3
Runeville <56156178+Runeville@users.noreply.github.com>
2021-08-01 00:02:45 +0600

    Some basic handlers settings

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -7,8 +7,8 @@ from loader import dp
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
     text = ("Список команд: ",
-            "/start - Начать диалог",
-            "/help - Получить справку",
-            "/physics - Show physics")
+            "/start - Starts dialog with Jake",
+            "/help - Gives help information",
+            "/physics - Shows physics playlist")
     
     await message.answer("\n".join(text))
