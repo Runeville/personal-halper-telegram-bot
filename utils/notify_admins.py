d841d70ad16725da5d356124fc33232c5647ccaf
Runeville <56156178+Runeville@users.noreply.github.com>
2021-08-01 00:04:58 +0600

    Fixing mistake in string

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -8,7 +8,7 @@ from data.config import ADMINS
 async def on_startup_notify(dp: Dispatcher):
     for admin in ADMINS:
         try:
-            await dp.bot.send_message(admin, "Jake is asleep!")
+            await dp.bot.send_message(admin, "Jake is awake!")
 
         except Exception as err:
             logging.exception(err)
