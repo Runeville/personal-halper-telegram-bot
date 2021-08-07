cbde567628147cd6fa36bebd2379d5a65fc03155
Runeville <varkhipov2004@gmail.com>
2021-08-07 22:46:51 +0600

    Added command "/playlists" to be shown in /help and in menu of commands

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -9,7 +9,8 @@ async def bot_help(message: types.Message):
     text = ("You can send me those commands and I will execute them: ",
             "/start - Starts dialog with Jake",
             "/help - Gives help information",
+            "/convert - Converts currency",
             "/physics - Shows physics playlist",
-            "/convert - Converts currency")
+            "/playlists - Shows your playlists")
     
     await message.answer("\n".join(text))
